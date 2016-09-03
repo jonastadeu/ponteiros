58d482a1013f98a13df4acaf01580487f3abafc0
jonastadeu <jonasreichert@gmail.com>
2016-09-02 22:21:35 -0300

    programa funcional

--- main.c
+++ main.c
@@ -1,30 +1,52 @@
-//************************************************************************
-// Programa exemplo de utilizacao de ponteiros
+//******************************************************************************
+// Exemplo de uso de ponteiros
 //
-// Laboratorio de Algoritmos II
+// Programa utilizado como exemplo na disciplina de Laboratorio de Algoritmos II
 //
-// Autor: Jonas T. R.
-// Data: 02/09/2016
-//************************************************************************
+// Prof: Jonas T. Reichert
+// Data: 26/08/2016
+//******************************************************************************
 
 #include <stdio.h>
-#include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int Numero = 2;
-int *Ptr_Numero = &Numero;
-
-int main(int argc, char *argv[]) {
+int main ()
+{
+	// criacao e inicializacao das variaveis
+	int Variavel_1 = 1, Variavel_2 = 2;
+	char Controle = 'n', Descarga;
 	
-	printf ("Insira um numero: ");
+	// criacao dos ponteiros
+	int *Ponteiro_1 , *Ponteiro_2;
 	
-	//scanf ("%i", &Numero);
-	scanf ("%i", Ptr_Numero);
-	
-	//printf ("O numero inserido foi: %i\n", Numero);
-	printf ("O numero inserido foi: %i\n", *Ptr_Numero);
-	
+	// enquanto
+	while (1)
+	{
+		// inicializacao dos ponteiros
+		Ponteiro_1 = &Variavel_1; // Ponteiro_1 recebe o endereco de Variavel_1
+		Ponteiro_2 = &Variavel_2; // Ponteiro_2 recebe o endereco de Variavel_2
+		
+		// exibe o valor contido em cada variavel a partir do endereco contido 
+		// nos ponteiros
+		printf ("\nVariavel_1 = %i", *Ponteiro_1);
+		printf ("\nVariavel_2 = %i", *Ponteiro_2);
+		
+		// exibe a solicitacao de controle
+		printf ("\n\nDeseja finalizar o programa? (s/n)");
+		// efetua a leitura do comando
+		scanf ("%c", &Controle);
+		// se o comando indicar a finalizacao, finaliza o loop
+		if (Controle == 's') break;
+		
+		// exibe a solicitacao de um valor
+		printf ("\nDigite um valor para a Variavel_1: ");
+		// efetua a leitura do valor e transfere o valor para a Variavel_1
+		scanf ("%i", Ponteiro_1);
+		// exibe a solicitacao de um valor
+		printf ("Digite um valor para a Variavel_2: ");
+		// efetua a leitura do valor e transfere o valor para a Variavel_2
+		scanf ("%i%c", Ponteiro_2, &Descarga);
+	}
+	// retorna 0
 	return 0;
 }
 
